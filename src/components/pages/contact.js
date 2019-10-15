fe86b1c4813958882ae3bfc5b003a4c52aeeb63f
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-15 16:48:41 -0600

    Added a few cleanups and TODO to production
    Added name and email to contact details, ommitted phone number.  TODO
    make contact details link with click to edit function.

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import contactImage from "../../../static/assets/images/contact/contact.jpg";
+import contactImage from "../../../static/assets/images/contact/contact.png";
 
 export default function() {
   return (
@@ -16,22 +16,22 @@ export default function() {
       <div className="right-column">
         <div className="contact-info">
           <div className="contact-detail">
-            <FontAwesomeIcon className="icon" icon="user-ninja" /> Your Name
+            <FontAwesomeIcon className="icon" icon="user-ninja" />Daniel Quinlog
+          </div>
+          {/* <div className="contact-detail">
+            <FontAwesomeIcon className="icon" icon="phone-alt" />801-687-9878
+          </div> */}
+          <div className="contact-detail">
+            <FontAwesomeIcon className="icon" icon="envelope" />dan.quinlog@gmail.com
           </div>
           <div className="contact-detail">
-            <FontAwesomeIcon className="icon" icon="phone-alt" /> Your Phone
+            <FontAwesomeIcon className="icon" icon={['fab', 'linkedin']} />Linkedin
           </div>
           <div className="contact-detail">
-            <FontAwesomeIcon className="icon" icon="envelope" /> youremail@gmail.com
+            <FontAwesomeIcon className="icon" icon={['fab', "twitter-square"]} />Twitter
           </div>
           <div className="contact-detail">
-            <FontAwesomeIcon className="icon" icon={['fab', 'linkedin']} /> LinkedIn
-          </div>
-          <div className="contact-detail">
-            <FontAwesomeIcon className="icon" icon={['fab', "twitter-square"]} /> Twitter
-          </div>
-          <div className="contact-detail">
-            <FontAwesomeIcon className="icon" icon={['fab', "instagram"]} /> instagram
+            <FontAwesomeIcon className="icon" icon={['fab', "instagram"]} />Instagram
           </div>
         </div>
       </div>
